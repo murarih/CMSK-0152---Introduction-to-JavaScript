265c7bdce4e4b43d31ac0f038d9623bd8f9ff72c
Harika <murarih@mymacewan.ca>
2023-03-23 17:30:57 -0600

    Execise checkin

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 document.getElementById('submitButton').addEventListener('click', function()
 {
+    var x = document.getElementById("newListItem").value;
     //get value from newListItem   
     //call addItem
+    addItem(x);
 });
 
 document.getElementById('setListColorButton').addEventListener('click', function()
 {
+    var newcolor=document.getElementById("newListColor").value;
     //get value from newListColor
     //call updateListColor
+    updateListColor(newcolor);
 });
 
 
 function addItem(newItemValue)
 {
-    var list;// = get itemCollection
+    var list = document.getElementById("itemCollection");// = get itemCollection
     var newItem = document.createElement('li');
     newItem.appendChild(document.createTextNode(newItemValue));
     list.appendChild(newItem);
@@ -21,6 +25,8 @@ function addItem(newItemValue)
 
 function updateListColor(newColor)
 {
+    var y = document.getElementById("itemCollection");
     //get itemCollection
     //set style color with newColor
+    y.style.color=newColor;
 }
